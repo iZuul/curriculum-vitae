afd194dd03d3ba987c843d90dc3c8fc362ffada6
Dhiaulhaq <muhammad.dhiaulhaq@spesolution.com>
2021-03-17 16:53:41 +0700

    change collapse component to card component, and implement props value

--- components/ListWorks.vue
+++ components/ListWorks.vue
@@ -1,75 +1,45 @@
 <template>
   <div>
-    <vs-collapse type="margin" class="vs-collapse-open-fullheight">
-      <vs-collapse-item open>
-        <div slot="header">
-          <h4>My Work</h4>
-        </div>
-        <div :style="styleWorks">
-          <vs-row>
-            <vs-col :vs-w="columnCardWork" class="px-3 pb-2 py-4" v-for="(item, index) in listProject" :key="index">
-              <vs-card fixedHeight class="zoom">
-                <div slot="header">
-                  <h3>
-                    {{ item.title }}
-                  </h3>
-                </div>
-                <div slot="media">
-                  <img :src="item.image" :alt="item.title">
-                </div>
-                <div>
-                  <span>{{ item.description }}</span>
-                </div>
-                <div slot="footer">
-                  <vs-row vs-justify="flex-end">
-                    <vs-button :href="item.link" :aria-label="`open-${item.shortName}`" type="gradient" color="primary" icon="arrow_forward" target></vs-button>
-                  </vs-row>
-                </div>
-              </vs-card>
-            </vs-col>
-          </vs-row>
-        </div>
-     </vs-collapse-item>
-    </vs-collapse>
+    <vs-card>
+      <div slot="header">
+        <h4>My Work</h4>
+      </div>
+      <div :style="styleWorks">
+        <vs-row>
+          <vs-col :vs-w="columnCardWork" class="px-3 pb-2 py-4" v-for="({ content }, index) in listWorks" :key="index">
+            <vs-card fixedHeight class="zoom">
+              <div slot="header">
+                <h3>
+                  {{ content.title }}
+                </h3>
+              </div>
+              <div slot="media">
+                <img :src="content.image.filename" :alt="content.image.alt">
+              </div>
+              <div>
+                <span>{{ content.short_description }}</span>
+              </div>
+              <div slot="footer">
+                <vs-row vs-justify="flex-end">
+                  <a v-if="content.link.url" :href="content.link.url" target="_blank">
+                    <vs-button :href="content.link" :aria-label="`open-${content.shortName}`" type="gradient" color="primary" icon="arrow_forward" target></vs-button>
+                  </a>
+                </vs-row>
+              </div>
+            </vs-card>
+          </vs-col>
+        </vs-row>
+      </div>
+    </vs-card>
   </div>
 </template>
 
 <script>
-import imageNanimeScraper from '@/static/images/nanime-scraper.png'
-import imageNanimeScraperAPI from '@/static/images/nanime-scraper-api.png'
-import imageMedicalWebApp from '@/static/images/medical-web-app.png'
-
 export default {
   props:{
     columnCardWork: Number,
-    styleWorks: String
+    styleWorks: String,
+    listWorks: Array
   },
-  data() {
-    return {
-      listProject: [
-        {
-          title: 'Nanime Scraper',
-          image: imageNanimeScraper,
-          description: 'Built with Vue, Bootswatch, and Express API',
-          link: 'https://nanime-scraper.netlify.app',
-          shortName: 'nanime-scraper'
-        },
-        {
-          title: 'Nanime Scraper API',
-          image: imageNanimeScraperAPI,
-          description: 'Built with Express, Cherrio, and node-fetch',
-          link: 'https://server-nanime-scraper-api.herokuapp.com',
-          shortName: 'nanime-scraper-api'
-        },
-        {
-          title: 'Medical Web App',
-          image: imageMedicalWebApp,
-          description: 'Built with Vue, Vuetify and Firebase',
-          link: 'https://pasienku.netlify.app',
-          shortName: 'medical-web-app'
-        }
-      ]
-    }
-  }
 }
 </script>
